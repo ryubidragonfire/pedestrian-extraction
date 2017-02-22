50a74a5592fe6ee862a8f018c87b2c80722005af
ryubidragonfire <chew.yean.yam@gmail.com>
2017-02-22 15:36:33 +0000

    try

---
+++ try.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pickle
+
+filename = './data/val_annotations.pkl'
+
+anno = pickle.load(open(filename, "rb" ))
