7a4de6ed28df6fea95d48b56704157e20a0bb106
ryubidragonfire <chew.yean.yam@gmail.com>
2017-03-01 07:57:03 +0000

    anno-06

--- try.py
+++ try.py
@@ -10,3 +10,5 @@ import pickle
 filename = './data/val_annotations.pkl'
 
 anno = pickle.load(open(filename, "rb" ))
+
+# https://www.haykranen.nl/2016/02/13/handling-complex-nested-dicts-in-python/
